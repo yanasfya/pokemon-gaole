7c9b9cd692df53dea035f2d91aa49f275cddb74a
yanasfya <137810000+yanasfya@users.noreply.github.com>
2024-11-19 01:55:25 +0800

    initial commit

---
+++ src/PlayerRecord.java
@@ -0,0 +1,42 @@
+public class PlayerRecord {
+	
+	//attributes
+	private int playerID;
+	private String playerName;
+	private int playerScores;
+	private int playerGaoleMedal;
+
+	//methods
+	public int getPlayerID() {
+		return playerID;
+	}
+	
+	public void setPlayerID(int PlayerID) {
+		this.playerID = PlayerID;
+	}
+	
+	public String getPlayerName() {
+		return playerName;
+	}
+	
+	public void setPlayerName(String PlayerName) {
+		this.playerName = PlayerName;
+	}
+	
+	public int getPlayerScores() {
+		return playerScores;
+	}
+	
+	public void setPlayerScores(int PlayerScores) {
+		this.playerScores = PlayerScores;
+	}
+	
+	public int getPlayerGaoleMedal() {
+		return playerGaoleMedal;
+	}
+	
+	public void setPlayerGaoleMedal(int PlayerGaoleMedal) {
+		this.playerGaoleMedal = PlayerGaoleMedal;
+	}
+
+}
